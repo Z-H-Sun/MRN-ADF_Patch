5898088f2704d7f53336c51efc2883aed422fd52
CS_CCME <48094808+Z-H-Sun@users.noreply.github.com>
2022-05-18 16:55:08 -0400

    add licenses (fix Z-H-Sun/CS_CCME_Posts#10)

--- MRN_for_Win/MestReNovaCrack.rb
+++ MRN_for_Win/MestReNovaCrack.rb
@@ -98,492 +98,71 @@ print "\nPlease choose the desired program to be patched by entering the number 
 l = STDIN.gets.to_i
 listNum = [l - 1] unless l.zero?
 
+contents = %Q(<mlic version="1.0">
+<licid>{0-0-0-0-0}</licid>
+<type>single</type>
+<hostid>0-0-0-0</hostid>
+<vendor>Mestrelab Research S.L.</vendor>
+<purchaser>cs@ccme</purchaser>
+<product>%s</product>
+<version>%s</version>
+<uuid>%s</uuid>
+<count>unlimited</count>
+<issued>2000-01-01</issued>
+<expires>never</expires>
+<update>forever</update>
+<sign>cnUtYm9hcmQ=</sign>
+</mlic>
+)
+data = {'AFFINImeter-NMR.lic' => ['AFFINImeter-NMR', '1.0', '{0BA83ECE-335C-404A-8226-070838EE6157}'],
+'Database.lic' => ['Database', '1.8', '{533B0BF3-4CE1-4f28-A8F8-E2403AF97030}'],
+'ElViS.lic' => ['ElViS', '1.3', '{D1956E14-B9AE-4B39-A507-A139A58688E3}'],
+'Mass.lic' => ['Mass', '1.8', '{5C1895EB-0054-40B1-BD42-192EF6589F83}'],
+'MBioHOS.lic' => ['MBioHOS', '1.0', '{F65FD237-2DC6-48DC-A5C4-653294F80B78}'],
+'Mbinding.lic' => ['Mbinding', '1.0', '{0514D764-3D21-464B-BD67-9CCA55AF6510}'],
+'MestrelabPredictor.lic' => ['Mestrelab Predictor', '1.8', '{FFB9B818-9D6B-4202-9C7E-B591E0C5C007}'],
+'MnovaBatchPhysChem-AAGAC.lic' => ['Mnova Batch PhysChem: Alpha Acid Glycoprotein Affinity Constant', '1.2', '{A989C2D7-2DD3-4F27-ABDE-B144BF89CB05}'],
+'MnovaBatchPhysChem-BBPC.lic' => ['Mnova Batch PhysChem: Blood-Brain-Partition Coefficient', '1.2', '{AD2E2594-012D-48AC-8A80-399F3C4A9BB7}'],
+'MnovaBatchPhysChem-FBF.lic' => ['Mnova Batch PhysChem: Free Brain Fraction', '1.2', '{AEFF80F9-4D8F-4DD4-90BE-7F28DC1A3478}'],
+'MnovaBatchPhysChem-FPF.lic' => ['Mnova Batch PhysChem: Free Plasma Fraction', '1.2', '{AC885539-E620-4DB1-BA8C-A44A50E897A0}'],
+'MnovaBatchPhysChem-HSAAC.lic' => ['Mnova Batch PhysChem: Human Serum Albumin Affinity Constant', '1.2', '{A8C4492C-FE86-47A2-83FF-0838868B6FEE}'],
+'MnovaBatchPhysChem-MAC.lic' => ['Mnova Batch PhysChem: Membrane Affinity Constant', '1.2', '{AA4BCD25-4337-42C2-834D-A8E14C0B614C}'],
+'MnovaBatchPhysChem-PPPB.lic' => ['Mnova Batch PhysChem: Per cent Plasma Protein Binding', '1.2', '{AB6F7B48-834B-40AB-B54B-DC2F0CB1CE74}'],
+'MnovaBatchPhysChem-Basic.lic' => ['Mnova Batch PhysChem Basic', '1.2', '{A7258B80-DF87-4C99-81EA-E44EC6468271}'],
+'MnovaBatchqNMR.lic' => ['Mnova Batch qNMR', '1.2.1.1740', '{B4A1DA63-354D-4B59-82DD-3266D4775F85}'],
+'MnovaBatchSMA.lic' => ['Mnova Batch SMA', '1.0.0.1926', '{08832918-A60B-11E4-89D3-123B93F75CBA}'],
+'MnovaBatchVerify.lic' => ['Mnova Batch Verify', '2.0', '{7161B159-6AC7-41DE-B3EA-7D703BA222C9}'],
+'MnovaGears.lic' => ['Mnova Gears', '1.0', '{3B25F857-8861-490F-8072-E1577A9FB350}'],
+'MnovaIUPACNameV1.0.lic' => ['Mnova IUPAC Name', '1.0', '{C3D1D7C0-A78F-44A0-ACE7-28EA5EA41CBC}'],
+'MnovaIUPACNameV1.14.lic' => ['Mnova IUPAC Name', '1.14', '{67AA7320-4992-433B-9EA3-4418C887E779}'],
+'MnovaqNMR.lic' => ['Mnova qNMR', '1.2.1.1740', '{5A01D4B0-7E6C-4C09-BF56-0DFCD96AFEFA}'],
+'MnovaScreen.lic' => ['Mnova Screen', '1.2', '{462E9690-D602-4857-A113-3E4B48DBF296}'],
+'MnovaSMA.lic' => ['Mnova SMA', '1.0.0.1937', '{F657C1C0-6675-11E3-949A-0800200C9A66}'],
+'MnovaStereoFitter.lic' => ['Mnova StereoFitter', '1.1', '{A4059C2F-E6C1-4F39-8AD3-89CA17966E9A}'],
+'MnovaVerify.lic' => ['Mnova Verify', '1.5', '{6F96FBD7-E418-4013-A9FA-B588F305C21E}'],
+'NMR.lic' => ['NMR', '1.8', '{5B0A2812-8EE2-406F-8030-84F5F6AC4A4B}'],
+'NMRPredictDesktop.lic' => ['NMRPredict Desktop', '1.8', '{1C479DD8-9A31-4016-AE56-DEE522080B06}'],
+'PhysChemProperties.lic' => ['PhysChem Properties', '1.8', '{63CA247B-FA1D-43cb-AAEE-AC0EF2448B9C}'],
+'RandomForestNMRPredictor.lic' => ['Random Forest NMR Predictor', '1.0', '{233C512F-83AB-4C4D-93E8-E8F31CEEFF5E}'],
+'ReactionMonitoring.lic' => ['Reaction Monitoring', '1.1.0.1443', '{F8559B7C-1F28-4D3C-8F0B-FAE1E87CB364}'],
+'StructureElucidation.lic' => ['Structure Elucidation', '1.0', '{6475DEBF-AB41-4FED-AE06-7996A427201C}']}
+
 for i in listNum
     filename = File.join(listDir[i], "MestReNova.exe")
     patch(filename)
 
-# ------------------------------------------------------------------------------
+    licenseDir = File.join(listDir[i], 'licenses')
+    Dir.mkdir(licenseDir) unless File.exists?(licenseDir)
+    puts "\n\e[1;33mReleasing .LIC files in\e[0m #{licenseDir}..."
 
-licenseDir = File.join(listDir[i], 'licenses')
-Dir.mkdir(licenseDir) unless File.exists?(licenseDir)
-puts "\n\e[1;33mReleasing .LIC files in\e[0m #{licenseDir}..."
-
-begin
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>AFFINImeter-NMR</product>
-<version>1.0</version>
-<uuid>{0ba83ece-335c-404a-8226-070838ee6157}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'AFFINImeter-NMR.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Database</product>
-<version>1.8</version>
-<uuid>{533B0BF3-4CE1-4f28-A8F8-E2403AF97030}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Database.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mass</product>
-<version>1.8</version>
-<uuid>{5c1895eb-0054-40b1-bd42-192ef6589f83}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Mass.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mbinding</product>
-<version>1.0</version>
-<uuid>{0514d764-3d21-464b-bd67-9cca55af6510}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mbinding_v1.0.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Alpha Acid Glycoprotein Affinity Constant</product>
-<version>1.2</version>
-<uuid>{A989C2D7-2DD3-4F27-ABDE-B144BF89CB05}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Alpha Acid Glycoprotein Affinity Constant.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Blood-Brain-Partition Coefficient</product>
-<version>1.2</version>
-<uuid>{AD2E2594-012D-48AC-8A80-399F3C4A9BB7}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Blood-Brain-Partition Coefficient.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Free Brain Fraction</product>
-<version>1.2</version>
-<uuid>{AEFF80F9-4D8F-4DD4-90BE-7F28DC1A3478}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Free Brain Fraction.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Free Plasma Fraction</product>
-<version>1.2</version>
-<uuid>{AC885539-E620-4DB1-BA8C-A44A50E897A0}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Free Plasma Fraction.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Human Serum Albumin Affinity Constant</product>
-<version>1.2</version>
-<uuid>{A8C4492C-FE86-47A2-83FF-0838868B6FEE}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Human Serum Albumin Affinity Constant.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Membrane Affinity Constant</product>
-<version>1.2</version>
-<uuid>{AA4BCD25-4337-42C2-834D-A8E14C0B614C}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Membrane Affinity Constant.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem: Per cent Plasma Protein Binding</product>
-<version>1.2</version>
-<uuid>{AB6F7B48-834B-40AB-B54B-DC2F0CB1CE74}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem - Per cent Plasma Protein Binding.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch PhysChem Basic</product>
-<version>1.2</version>
-<uuid>{A7258B80-DF87-4C99-81EA-E44EC6468271}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Batch PhysChem Basic.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch qNMR</product>
-<version>1.2.1.1740</version>
-<uuid>{B4A1DA63-354D-4B59-82DD-3266D4775F85}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Mnova Batch qNMR.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch SMA</product>
-<version>1.0.0.1926</version>
-<uuid>{08832918-A60B-11E4-89D3-123B93F75CBA}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Mnova Batch SMA.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{00000000-0000-0000-0000-00000000000}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Batch Verify</product>
-<version>2.0</version>
-<uuid>{7161B159-6AC7-41de-B3EA-7D703BA222C9}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova batch verify.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Gears</product>
-<version>1.0</version>
-<uuid>{3b25f857-8861-490f-8072-e1577a9fb350}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Gears_v1.0.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova IUPAC Name</product>
-<version>1.0</version>
-<uuid>{c3d1d7c0-a78f-44a0-ace7-28ea5ea41cbc}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova IUPAC Name_v1.0.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova qNMR</product>
-<version>1.2.1.1740</version>
-<uuid>{5A01D4B0-7E6C-4C09-BF56-0DFCD96AFEFA}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Mnova qNMR.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Screen</product>
-<version>1.2</version>
-<uuid>{462E9690-D602-4857-A113-3E4B48DBF296}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Mnova Screen.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova SMA</product>
-<version>1.0.0.1937</version>
-<uuid>{F657C1C0-6675-11E3-949A-0800200C9A66}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Mnova SMA.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Mnova Verify</product>
-<version>1.5</version>
-<uuid>{6f96fbd7-e418-4013-a9fa-b588f305c21e}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Mnova Verify.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>NMR</product>
-<version>1.8</version>
-<uuid>{5b0a2812-8ee2-406f-8030-84f5f6ac4a4b}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'NMR.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>NMRPredict Desktop</product>
-<version>1.8</version>
-<uuid>{1c479dd8-9a31-4016-ae56-dee522080b06}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'NMRPredict Desktop.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>PhysChem Properties</product>
-<version>1.8</version>
-<uuid>{63CA247B-FA1D-43cb-AAEE-AC0EF2448B9C}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'PhysChem Properties.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Random Forest NMR Predictor</product>
-<version>1.0</version>
-<uuid>{233c512f-83ab-4c4d-93e8-e8f31ceeff5e}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Random Forest NMR Predictor.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Reaction Monitoring</product>
-<version>1.1.0.1443</version>
-<uuid>{F8559B7C-1F28-4D3C-8F0B-FAE1E87CB364}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>
-)
-open(File.join(licenseDir, 'Reaction Monitoring.lic'), 'w') {|f| f.write(d)}
-
-d = %Q(<mlic version="1.0">
-<licid>{0-0-0-0-0}</licid>
-<type>single</type>
-<hostid>0-0-0-0</hostid>
-<vendor>Mestrelab Research S.L.</vendor>
-<purchaser>foss@ru-board</purchaser>
-<product>Structure Elucidation</product>
-<version>1.0</version>
-<uuid>{6475debf-ab41-4fed-ae06-7996a427201c}</uuid>
-<count>unlimited</count>
-<issued>2000-01-01</issued>
-<expires>never</expires>
-<update>forever</update>
-<sign>cnUtYm9hcmQ=</sign>
-</mlic>)
-open(File.join(licenseDir, 'Structure Elucidation_v1.0.lic'), 'w') {|f| f.write(d)}
-
-puts "\e[1;32mAll .LIC files have been created.\e[0m"
-rescue
-    puts "\e[1;31mError occurred:\e[0m"
-    puts $!.inspect
-    puts $@.inspect
-end
+    begin
+        data.keys.each {|i| open(File.join(licenseDir, i), 'w') {|f| f.write(contents % data[i])}}
+        puts "\e[1;32mAll .LIC files have been created.\e[0m"
+    rescue
+        puts "\e[1;31mError occurred:\e[0m"
+        puts $!.inspect
+        puts $@.inspect
+    end
 
 end
 exit
